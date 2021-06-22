9cd51c3eaf35f158bfec4f2732e9b6b3647cd528
Mathias Kahr <m.kahr@student.tugraz.at>
2021-06-22 13:40:45 +0200

    init

---
+++ client.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"errors"
+)
+
+type APIClient struct {
+}
+
+func NewClient() (*APIClient, error) {
+	return nil, errors.New("not implemented")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-p2pb2b
+
+go 1.16

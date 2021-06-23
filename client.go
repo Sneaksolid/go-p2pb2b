79f98ca3df2a80b256d2778d9a9f8668b0053c10
Mathias Kahr <m.kahr@student.tugraz.at>
2021-06-23 14:01:01 +0200

    update request headers

--- client.go
+++ client.go
@@ -3,7 +3,7 @@ package p2pb2b
 import (
 	"bytes"
 	"crypto/hmac"
-	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/base64"
 	"encoding/hex"
 	"encoding/json"
@@ -23,6 +23,7 @@ const API_URL = "https://api.p2pb2b.io"
 const API_KEY_HEADER = "X-TXC-APIKEY"
 const API_PAYLOAD_HEADER = "X-TXC-PAYLOAD"
 const API_SIGNATURE_HEADER = "X-TXC-SIGNATURE"
+const API_CONTENT_TYPE = "Content-Type"
 
 func NewClient(apikey, apiSecret string) *APIClient {
 	return &APIClient{
@@ -48,13 +49,14 @@ func (a *APIClient) request(endpoint string, request APIRequest) ([]byte, error)
 	}
 
 	payload := base64.StdEncoding.EncodeToString(b)
-	h := hmac.New(sha256.New, []byte(a.apiSecret))
+	h := hmac.New(sha512.New, []byte(a.apiSecret))
 	h.Write([]byte(payload))
 	signature := hex.EncodeToString(h.Sum(nil))
 
 	req.Header.Add(API_KEY_HEADER, a.apiKey)
 	req.Header.Add(API_PAYLOAD_HEADER, payload)
 	req.Header.Add(API_SIGNATURE_HEADER, signature)
+	req.Header.Add(API_CONTENT_TYPE, "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
